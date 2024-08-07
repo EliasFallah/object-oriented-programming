c6b3ccb59ed6c117f992335c3ddbbaba230fdac3
eliasfallahWSU <22111881@student.westernsydney.edu.au>
2024-08-07 06:02:12 +0000

    Complete Task3_2 and Task3_3

--- Prac_3/Task3_2n3.cpp
+++ Prac_3/Task3_2n3.cpp
@@ -10,6 +10,7 @@ void displayBoard(int board[][3]) {
 	cout << "   1    2    3" << endl << endl;
 	for (int i = 0; i < 3; i++) {
 		cout << i + 1;
+		// // Check player symbols in board and prints them
 		for (int j = 0; j < 3; j++) {
 			char playerSymbol = ' ';
 			if (board[i][j] == 1)
@@ -17,6 +18,7 @@ void displayBoard(int board[][3]) {
 			else if (board[i][j] == -1)
 				playerSymbol = 'O';
 			cout << setw(3) << playerSymbol;
+			// prints the middle wals of the board
 			if (j != 2)
 				cout << " |";
 		}
@@ -28,10 +30,32 @@ void displayBoard(int board[][3]) {
 }
 
 bool isValidMove(int board[][3], int x, int y) {
-	if (true) //Add your code here)
-		return true;
-	else
-		return false;
+	bool retval = false;
+	// Checks x is in correct range
+	if ((x >= 0) && (x < 3)) { //Add your code here)
+		// Checks y is in correct range
+		if ((y >= 0) && (y < 3)){
+			// Checks position has not already been filled
+			if ((board[x][y] == 0)) {
+				// return value method changed to allow for nested if statements and error message
+				retval = true;
+			}
+		} 		
+	} 
+	// else sattement removed due to redundency 
+
+	// else {
+	// 	retval = false;
+	// }
+	
+	if (retval == true){
+		return retval;
+	}else {
+		// added print statement for user clarity
+		cout << "Enter a valid move: ";
+		return retval;
+	}
+
 }
 
 bool getXOMove(int board[][3], int noOfMoves, int &x, int &y) {
@@ -59,14 +83,31 @@ int gameStatus(int board[][3], int &noOfMoves) {
 //Check rows for a win
 
 //Add your code here
-
+	for (int i = 0; i <=3; i++) {
+		if ((board[i][0] == board[i][1]) && (board[i][1] == board[i][2])){
+			return board[i][0];
+		}
+	}
 //Check columns for a win
 
 //Add your code here
+	for (int i = 0; i <= 3; i++) {
+		if ((board[0][i] == board[1][i]) && (board[1][i] == board[2][i])){
+			return board[0][i];
+		}
+	}
 
 //Check diagonals for a win
-
+	
 //Add your code here
+	// Checks left to right diagonal
+	if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])){
+		return board[0][0];
+	}
+	// Checks right to left diagonal
+	if ((board[0][2] == board[1][1]) && (board[1][1] == board[2][0])){
+		return board[0][2];
+	}
 
 	if (noOfMoves >= 9)
 		return 2;

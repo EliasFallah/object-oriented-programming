34e2031dac8bccf17bbe13048dfe404102c85cea
eliasfallahWSU <22111881@student.westernsydney.edu.au>
2024-09-22 12:09:46 +0000

    Complete task 6_1

--- Prac_6/task6_1.cpp
+++ Prac_6/task6_1.cpp
@@ -4,24 +4,35 @@ using namespace std;
 class A {
   int i;
 public:
-  A(int ii) : i(ii) {//Add a statement here}
-  ~A() {//Add a statement here}
+  A(int ii) : i(ii) {
+    cout << "A's ctor is called" << endl;
+  }
+  ~A() {
+    cout << "A's dtor is called" << endl;
+    }
   void f() const {}
 };
 
 class B {
   int i;
 public:
-  B(int ii) : i(ii) {//Add a statement here}
-  ~B() {//Add a statement here}
+  B(int ii) : i(ii) {
+    cout << "B's ctor is called" << endl;
+    }
+  ~B() {
+    cout << "B's dtor is called" << endl;
+    }
   void f() const {}
 };
 
 class C : public B {
   A a;
 public:
-  C(int ii) : B(ii), a(ii) {//Add a statement here}
-  ~C() {//Add a statement here} 
+  C(int ii) : B(ii), a(ii) {
+    cout << "C's ctor is called" << endl;}
+  ~C() {
+    cout << "C's dtor is called" << endl;
+  } 
   void f() const {  
     a.f();
     B::f();

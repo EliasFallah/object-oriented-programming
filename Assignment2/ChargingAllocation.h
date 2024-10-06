fbb51b926852338bfdbfe282151827f0bc94cbbe
eliasfallahWSU <22111881@student.westernsydney.edu.au>
2024-10-06 07:08:34 +0000

    Create ChargingAllocation.h

---
+++ Assignment2/ChargingAllocation.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Vehicle.h"
+
+using namespace std;
+
+class ChaargingAllocation {
+    public:
+        vector<Vehicle> vehicles;
+        ChaargingAllocation();
+};
+
+ChaargingAllocation::ChaargingAllocation() {
+
+}

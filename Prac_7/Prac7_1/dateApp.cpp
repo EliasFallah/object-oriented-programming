b7ff5fc6154b157e35c7fdebb9e2259c3f65b07b
eliasfallahWSU <22111881@student.westernsydney.edu.au>
2024-10-07 07:09:38 +0000

    Add Prac_7 starting files

---
+++ Prac_7/Prac7_1/dateApp.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<ctime>
+#include <fstream>
+#include <cstdlib>
+#include<vector>
+
+#include "Date.h"
+
+using namespace std;
+
+int main() {
+
+	ifstream fin;
+
+	//Read a date from the file
+	fin.open("date_in.txt");
+	if (fin.fail()) {
+		cout << "Input file opening failed.\n";
+		exit(1);
+	}
+
+	char deliminator;
+	int day = 0;
+	int month = 0;
+	int year = 0;
+
+	fin >> day;
+	fin.get(deliminator);
+	fin >> month;
+	fin.get(deliminator);
+	fin >> year;
+	fin.get(deliminator);
+
+	Date d(day, month, year);
+	cout << d << endl;
+
+	//output the date to a file
+	ofstream fout;
+
+	fout.open("date_out.txt");
+	if (fout.fail()) {
+		cout << "Output file opening failed.\n";
+		exit(1);
+	}
+
+	fout << d << endl;
+	return 0;
+}

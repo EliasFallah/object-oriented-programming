736133210adcee57166fd447cb5d846f2d55f4d1
eliasfallahWSU <22111881@student.westernsydney.edu.au>
2024-10-20 00:43:02 +0000

    Add decleration comment

--- Assignment2_python/random_demand.py
+++ Assignment2_python/random_demand.py
@@ -1,3 +1,10 @@
+# DECLARATION
+# I hold a copy of this assignment that I can produce if the original is lost or damaged.
+# I hereby certify that no part of this assignment has been copied from any other student’s work or
+# from any other source except where due acknowledgement is made in the assignment. No part
+# of this assignment has been written/produced for me by another person except where such
+# collaboration has been authorised by the subject lecturer concerned.
+
 from demand_generator import DemandGenerator
 from charging_allocation import ChargingAllocation
 

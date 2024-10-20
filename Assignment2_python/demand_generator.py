c4631eb9aa7f8f1afa1c655af1aadbf5aca70252
eliasfallahWSU <22111881@student.westernsydney.edu.au>
2024-10-20 00:38:54 +0000

    Update comments

--- Assignment2_python/demand_generator.py
+++ Assignment2_python/demand_generator.py
@@ -3,9 +3,9 @@ import constants
 
 class DemandGenerator:
     def __init__(self):
-        random.seed()  # Initialize random number generator
+        random.seed()  
         self.file_name = "NewChargingDemands.txt"
-        self.num_demands = random.randint(350, 400)  # Generate a random number of demands between 350 and 400
+        self.num_demands = random.randint(350, 400)  
 
         with open(self.file_name, "w") as charging_demands:
             for i in range(200, self.num_demands):
@@ -17,6 +17,6 @@ class DemandGenerator:
 
     def new_demand(self, num):
         self.vehicle_id = num
-        self.destination_id = random.randint(1, constants.NUM_CITIES - 1)  # Random destination between 1 and NUM_CITIES-1
-        self.capacity_range = random.randint(350, 550)  # Random capacity between 350 and 550
-        self.remain_range = random.randint(constants.MIN_REMAIN_RANGE, self.capacity_range)  # Remain range
+        self.destination_id = random.randint(1, constants.NUM_CITIES - 1)  
+        self.capacity_range = random.randint(350, 550)  
+        self.remain_range = random.randint(constants.MIN_REMAIN_RANGE, self.capacity_range)  
--- Assignment2_python/random_demand.py
+++ Assignment2_python/random_demand.py
@@ -2,12 +2,12 @@ from demand_generator import DemandGenerator
 from charging_allocation import ChargingAllocation
 
 def main():
-    demand_gen = DemandGenerator()  # Generate new charging demands
-    charging_alloc = ChargingAllocation()  # Initialize charging allocation
-    charging_alloc.display_vehicles()  # Display vehicle information
-    charging_alloc.display_chargers()  # Display charger information
-    charging_alloc.allocate_charging()  # Allocate charging for vehicles
-    charging_alloc.print_charging()  # Print the charging allocation details
+    demand_gen = DemandGenerator() 
+    charging_alloc = ChargingAllocation() 
+    charging_alloc.display_vehicles() 
+    charging_alloc.display_chargers()  
+    charging_alloc.allocate_charging()  
+    charging_alloc.print_charging()  
 
 if __name__ == "__main__":
     main()
